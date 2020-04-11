d75634160172389116bc1a161a206baf6c5752b6
Rohit Dhingra <Rohit Dhingra@DESKTOP-TQ58HNN>
2020-04-12 01:43:03 +0530

    ELK Stack POC

---
+++ elk-stack-example/src/main/java/com/example/elk/stack/User.java
@@ -0,0 +1,15 @@
+package com.example.elk.stack;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class User {
+private int id;
+private String name;
+}
